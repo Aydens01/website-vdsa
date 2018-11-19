97b5ce4803086b09b31115bb41af77b621f9c695
lafageadri <lafageadri@eisti.eu>
2018-11-19 10:30:11 +0100

    Implémentation d'une fonction basique pour la création de cercle sur une carte.
    Bug fixed : le fichier view/test/map.php est à présent ignoré.

--- public/js/custom/map.js
+++ public/js/custom/map.js
@@ -1,27 +1,31 @@
-/* Création d'une map random
+/*TODO: recupération des données avec ajax*/
+
+/*TODO: fonction qui fait des cercles sur la carte*/
+function setCircle(map, center, radius) {
+    var circle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: center,
+        radius:radius
+    });
+}
+
+/*TODO: fonction qui affiche des indications au passage du curseur*/
+
+/* Création d'une map random */
 var map;
+/* Variables de test */
+var ctr = {lat: -34.397, lng: 150.644};
+var rad = 50000;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
     });
+    setCircle(map, ctr, rad);
 }
-*/
-
-
-
-/* Variables de test */
-var ctr = {lat: 34.397, lng: 150.644};
-var rad = 5;
-
-/*TODO: recupération des données avec ajax*/
-
-/*TODO: fonction qui fait des cercles sur la carte*/
-function setCircle(center , radius) {
-    console.log(center, radius);
-}
-
-/*TODO: fonction qui affiche des indications au passage du curseur*/
-
-setCircle(ctr, rad);
